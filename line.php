ffb48f79f7b8f79a9f3388db0e8837b5f821a1c2
SawyerRoP <netsmut59@gmail.com>
2018-02-12 10:22:27 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'UBiM3HYXHiyEgFEZqWwsTYy2eiU2pOGWpbVqdPtLR4B7o0Inm8OAKahx46Pu8yumqXPDYq3LOlaLfEJ8k5WAQMS8nX7oyH0ZGixTg6GWxo/LPbnJvcISI0dG/n5BjTm5DEVFg7mojbRrf9hw7QP2NgdB04t89/1O/w1cDnyilFU=';
+$access_token = '';
 
 // Get POST body content
 $content = file_get_contents('php://input');
